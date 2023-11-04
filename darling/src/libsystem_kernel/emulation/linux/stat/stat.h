1e0220052d0844ec56ce944da80fb60f752cb447
Thomas A <curioustommy@protonmail.com>
2023-11-04 12:05:40 -0700

    Rework "stat" code to better support ARM64
    Unlike i386/x86_64, ARM64 doesn't contain a `stat64` struct. On top of that, ARM64's `stat` is actually the same as `stat64`. As a result, some of the stat code will need to be converted to handle this scenario.
    
    For more details, I recommend looking at the `[xnu]/bsd/sys/stat.h` file.

--- darling/src/libsystem_kernel/emulation/linux/stat/stat.h
+++ darling/src/libsystem_kernel/emulation/linux/stat/stat.h
@@ -5,7 +5,12 @@ struct stat;
 struct stat64;
 
 long sys_stat(const char* path, struct stat* stat);
+
+#if defined(__i386__) || defined(__x86_64__)
 long sys_stat64(const char* path, struct stat64* stat);
+#elif defined(__arm64__)
+long sys_stat64(const char* path, struct stat* stat);
+#endif
 
 #endif
 
