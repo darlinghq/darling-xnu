626ffcb0970f7d4daafdb0fb58b469a1b7d34207
Thomas A. <curioustommy@protonmail.com>
2024-06-04 19:51:10 -0700

    Have LINUX_O_* Constants Better Match Offical Implementation
    If you look at <bits/fcntl-linux.h>, you'll notice that some constants will only be defined if they have not already been defined. This is important for some architextures (such as ARM64, which have custom implementations for some of the constants)

--- darling/src/libsystem_kernel/emulation/linux/fcntl/open.h
+++ darling/src/libsystem_kernel/emulation/linux/fcntl/open.h
@@ -4,21 +4,70 @@
 long sys_open(const char* filename, int flags, unsigned int mode);
 long sys_open_nocancel(const char* filename, int flags, unsigned int mode);
 
+
+// Based on <bits/fcntl-linux.h>
+#define LINUX_O_ACCMODE 0003
 #define LINUX_O_RDONLY 00
 #define LINUX_O_WRONLY 01
 #define LINUX_O_RDWR 02
+
+#ifndef LINUX_O_CREAT
 #define LINUX_O_CREAT 0100
+#endif
+#ifndef LINUX_O_EXCL
 #define LINUX_O_EXCL 0200
+#endif
+#ifndef LINUX_O_NOCTTY
 #define LINUX_O_NOCTTY 0400
+#endif
+#ifndef LINUX_O_TRUNC
 #define LINUX_O_TRUNC 01000
+#endif
+#ifndef LINUX_O_APPEND
 #define LINUX_O_APPEND 02000
+#endif
+#ifndef LINUX_O_NONBLOCK
 #define LINUX_O_NONBLOCK 04000
+#endif
+#ifndef LINUX_O_NDELAY
+#define LINUX_O_NDELAY O_NONBLOCK
+#endif
+#ifndef LINUX_O_SYNC
 #define LINUX_O_SYNC 04010000
+#endif
+#ifndef LINUX_O_ASYNC
 #define LINUX_O_ASYNC 020000
-#define LINUX_O_CLOEXEC 02000000
+#endif
+#ifndef LINUX_O_LARGEFILE
 #define LINUX_O_LARGEFILE 0100000
-#define LINUX_O_NOFOLLOW 0400000
+#endif
+#ifndef LINUX_O_DIRECTORY
 #define LINUX_O_DIRECTORY 0200000
+#endif
+#ifndef LINUX_O_NOFOLLOW
+#define LINUX_O_NOFOLLOW 0400000
+#endif
+#ifndef LINUX_O_CLOEXEC
+#define LINUX_O_CLOEXEC 02000000
+#endif
+#ifndef LINUX_O_DIRECT
+#define LINUX_O_DIRECT 040000
+#endif
+#ifndef LINUX_O_NOATIME
+#define LINUX_O_NOATIME 01000000
+#endif
+#ifndef LINUX_O_PATH
+#define LINUX_O_PATH 010000000
+#endif
+#ifndef LINUX_O_DSYNC
+#define LINUX_O_DSYNC 010000
+#endif
+#ifndef LINUX_O_TMPFILE
+#define LINUX_O_TMPFILE (020000000 | LINUX_O_DIRECTORY)
+#endif
+
+#define LINUX_O_FSYNC LINUX_O_SYNC
+
 
 #define BSD_O_RDONLY 0
 #define BSD_O_WRONLY 1
